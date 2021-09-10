2fa36b0e740c47aa5c907dbbe550bcc9786e459a
unknown <maivaldc2@gmail.com>
2021-09-10 19:07:09 -0400

    get request

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -110,6 +110,21 @@ const ProfileScreen = ({route, navigation}) => {
           onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
           style={StyleSheet.absoluteFillObject}
         />
+        <View style={{ flex: 1, padding: 24 }}>
+      {isLoading ? <Text>Loading...</Text> : 
+      ( <View style={{ flex: 1, flexDirection: 'column', justifyContent:  'space-between'}}>
+          <Text style={{ fontSize: 18, color: 'green', textAlign: 'center'}}>{data.title}</Text>
+          <Text style={{ fontSize: 14, color: 'green', textAlign: 'center', paddingBottom: 10}}>Articles:</Text>
+          <FlatList
+            data={data.articles}
+            keyExtractor={({ id }, index) => id}
+            renderItem={({ item }) => (
+              <Text>{item.id + '. ' + item.title}</Text>
+            )}
+          />
+        </View>
+      )}
+    </View>
       </View>
   );
           }
